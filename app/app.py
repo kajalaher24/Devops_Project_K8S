7572fb5524606ea489cb556bae6bdefb036aaeca
Kajal Aher <kajalaher24@gmail.com>
2025-11-13 00:55:53 +0530

    Added ConfigMap and Secret integration for Flask app

--- app/app.py
+++ app/app.py
@@ -1,14 +1,18 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to DevOps Project with Kubernetes!"
+    app_env = os.getenv("APP_ENV", "development")
+    app_version = os.getenv("APP_VERSION", "v1")
+    return f"Welcome to DevOps Project with Kubernetes!<br>Environment: {app_env}<br>Version: {app_version}"
 
 @app.route('/about')
 def about():
-    return "This is a Flask app deployed on Kubernetes."
+    db_password = os.getenv("DB_PASSWORD", "not_set")
+    return f"This is a Flask app deployed on Kubernetes.<br>DB Password: {db_password}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
